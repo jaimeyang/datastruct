45402f621e6e28099a9be7fe44b503edb1dc4483
jaimeyang <jaimeyang1990@gmail.com>
2020-09-15 00:26:23 +0800

    begin writing the question

---
+++ sort/Question.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jaime on 2020/9/15.
+//
+
+#include "Question.h"
---
+++ sort/Question.h
@@ -0,0 +1,14 @@
+//
+// Created by jaime on 2020/9/15.
+//
+
+#ifndef SORT_QUESTION_H
+#define SORT_QUESTION_H
+
+
+class Question {
+
+};
+
+
+#endif //SORT_QUESTION_H
