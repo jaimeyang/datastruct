20e978570adadbd0a8ca0b4881e6de4777f60776
jaimeyang1990@gmail.com <jaimeyang1990@gmail.com>
2020-09-15 17:41:49 +0800

    search project

---
+++ search/Search.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TestAdmin on 2020/9/15.
+//
+
+#include "Search.h"
---
+++ search/Search.h
@@ -0,0 +1,14 @@
+//
+// Created by TestAdmin on 2020/9/15.
+//
+
+#ifndef SEARCH_SEARCH_H
+#define SEARCH_SEARCH_H
+
+
+class Search {
+
+};
+
+
+#endif //SEARCH_SEARCH_H
